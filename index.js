dcc4caa4ccc298061bb2ff62682dc822d429d2b7
Henrincode <spellctro@gmail.com>
2025-11-13 20:17:51 -0300

    feat: preparando estrutura de rotas

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    // ...
+})
+
+app.post('/', (req, res) => {
+    // ...
+})
+
+app.listen(3000)
